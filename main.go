1aa3148f15a277f50737781aece07f69c114976a
Alan Gerber <alan@knewton.com>
2016-12-23 18:51:39 -0600

    Update to handle event server and clients

--- main.go
+++ main.go
@@ -3,34 +3,51 @@ package main
 import ( "log"; "net" )
 
 func main() {
-    eventSrc, err := net.Listen("tcp", ":9090")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for {
-        conn, err := eventSrc.Accept()
-        if err != nil {
-            log.Println(err)
-            continue
-        }
-        go handleConnection(conn)
-    }
+	log.SetOutput(os.Stdout)
+
+	eventSrc, err := net.Listen("tcp", ":9090")
+	userClients, err := net.Listen("tcp", ":9099")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		eventsConn, err := eventSrc.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleEvents(eventsConn)
+
+		clientConn, err := userClients.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleClient(clientConn)
+	}
 }
 
-func handleConnection(c net.Conn) {
-    buf := make([]byte, 4096)
+func handleEvents(c net.Conn) {
+	buf := make([]byte, 4096)
 
-    for {
-        n, err:= c.Read(buf)
-        if err != nil || n == 0 {
-            c.Close()
-            break
-        }
-        n, err = c.Write(buf[0:n])
-        if err != nil {
-            c.Close()
-            break
-        }
-        log.Printf("Connection from %v closed.", c.RemoteAddr())
-    }
+	for {
+		n, err := c.Read(buf)
+		if err != nil || n == 0 {
+			c.Close()
+			break
+		}
+		log.Printf("%s", buf[0:n])
+		//log.Printf("Connection from %v closed.", c.RemoteAddr())
+	}
+}
+
+func handleClient(c net.Conn) {
+	for {
+		_, err := c.Write([]byte("TODO hey man"))
+		if err != nil {
+			c.Close()
+			break
+		}
+		//log.Printf("Connection from %v closed.", c.RemoteAddr())
+	}
 }
